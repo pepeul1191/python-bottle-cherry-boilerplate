c0a97d73606691174eb39f01c3884794107547b7
Pepe Valdivia <jvaldivia@softweb.pe>
2019-01-22 17:20:41 -0500

    Arrancando aplicación con cherrypy

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from bottle import route, run, template
+import time
+from bottle import route, run, template, install, response, hook
 
 
 @route('/hello/<name>')
 def index(name):
     return template('<b>Hello {{name}}</b>!', name=name)
+
+
+@hook('after_request')
+def separator():
+    response.headers['server'] = 'Ubuntu; CherryPy\\8.9.1;'
 
 
 run(host='localhost',
