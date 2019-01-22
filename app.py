e992ac9f631442eb785cdf5100f5a39719c42684
Pepe Valdivia <jvaldivia@softweb.pe>
2019-01-22 16:30:54 -0500

    Añadiendo ejecución con servidor cherry

--- app.py
+++ app.py
@@ -9,4 +9,8 @@ def index(name):
     return template('<b>Hello {{name}}</b>!', name=name)
 
 
-run(host='localhost', port=8080)
+run(host='localhost',
+    port=8080,
+    server='cherrypy',
+    debug=True,
+    reloader=True)
