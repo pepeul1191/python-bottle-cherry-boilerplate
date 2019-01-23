aa8e8cb90119cc2d89ab97272634dad636eba23b
Pepe Valdivia <jvaldivia@softweb.pe>
2019-01-22 19:14:07 -0500

    Routes en otros archivos

--- app.py
+++ app.py
@@ -1,23 +1,29 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import time
-from datetime import datetime
-from functools import wraps
-from bottle import route, run, template, install, response, hook, request
+from bottle import Bottle, template, run
 from configs.middlewares import logs, headers
+from routes.demo import route as _demo
+
+app = Bottle()
+# install middlewares
+app.install(logs)
+app.install(headers)
 
 
-@route('/hello/<name>')
-def index(name):
+@app.route('/hello/<name>')
+def hello(name):
     return template('<b>Hello {{name}}</b>!', name=name)
 
 
-install(logs)
-install(headers)
-
-run(host='localhost',
-    port=8080,
-    server='cherrypy',
-    debug=True,
-    reloader=True)
+if __name__ == '__main__':
+    # routes
+    app.mount('/demo', _demo)
+    # start app
+    run(
+        app,
+        host='localhost',
+        port=8080,
+        server='cherrypy',
+        debug=True,
+        reloader=True)
---
+++ routes/demo.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import json
+from bottle import Bottle, HTTPResponse
+from configs.middlewares import logs, headers
+
+route = Bottle()
+# install middlewares
+route.install(logs)
+route.install(headers)
+
+
+@route.route('/api', method='GET')
+def listar():
+    rpta = None
+    status = 200
+    try:
+        rpta = '=)'
+    except Exception as e:
+        rpta = {
+            'tipo_mensaje': 'error',
+            'mensaje': [
+                'Se ha producido un error en listar los sistemas',
+                str(e)
+            ],
+        }
+        status = 500
+    return HTTPResponse(status=status, body=json.dumps(rpta))
